e6da9b572ddd304ed4f45f8d6d1db7f6400e35af
serhiipetrovskyi <serhiypetrovskyy@gmail.com>
2024-02-04 15:20:55 +0200

    settings.py is updated for deployment on Azure

--- WeatherReminder/WeatherReminder/settings.py
+++ WeatherReminder/WeatherReminder/settings.py
@@ -10,32 +10,91 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-
+#import io
 from datetime import timedelta
 
 from pathlib import Path
+from dotenv import load_dotenv
+#from urllib.parse import urlparse
+#from google.cloud import secretmanager
+#import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
+
+SECURE_SSL_REDIRECT = \
+    os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
+if SECURE_SSL_REDIRECT:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DBNAME'),
+        'HOST': os.environ.get('DBHOST'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS'),
+        'OPTIONS': {'sslmode': 'require'},
+    }
+}
+# env = environ.Env(DEBUG=(bool, False))
+# env_file = os.path.join(BASE_DIR, '.env')
+#
+# if os.path.isfile(env_file):
+#     # read a local .env file
+#     env.read_env(env_file)
+# elif os.environ.get('GOOGLE_CLOUD_PROJECT', None):
+#     # pull .env file from Secret Manager
+#     project_id = os.environ.get('GOOGLE_CLOUD_PROJECT')
+#
+#     client = secretmanager.SecretManagerServiceClient()
+#     settings_name = os.environ.get('SETTINGS_NAME', 'django_settings')
+#     name = f'projects/{project_id}/secrets/{settings_name}/versions/latest'
+#     payload = client.access_secret_version(name=name).payload.data.decode('UTF-8')
+#
+#     env.read_env(io.StringIO(payload))
+# else:
+#     raise Exception('No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)wwzle+nh#(ep5t!+$g*yr=fwvke#@*0hcv&efuer(#6!08n*6'
+# SECRET_KEY = env('SECRET_KEY')
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# #DEBUG = env('DEBUG')
+# DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS settings
+#
+# APPENGINE_URL = env('APPENGINE_URL', default=None)
+# if APPENGINE_URL:
+#     # ensure a scheme is present in the URL before it's processed.
+#     if not urlparse(APPENGINE_URL).scheme:
+#         APPENGINE_URL = f'https://{APPENGINE_URL}'
+#
+#     ALLOWED_HOSTS = [urlparse(APPENGINE_URL).netloc]
+#     CSRF_TRUSTED_ORIGINS = [APPENGINE_URL]
+#     SECURE_SSL_REDIRECT = True
+# else:
+#     ALLOWED_HOSTS = ['*']
 
 # Application definition
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
-    #'subscription.apps.SubscriptionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,16 +144,38 @@ TEMPLATES = [
 WSGI_APPLICATION = 'WeatherReminder.wsgi.application'
 
 
-# Database
+# Database setting for local development
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# Database
+# [START db_setup]
+# [START gaestd_py_django_database_config]
+# Use django-environ to parse the connection string
+# DATABASES = {"default": env.db()}
+# # # If the flag as been set, configure to use proxy
+# if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+#     DATABASES["default"]["HOST"] = "127.0.0.1"
+#     DATABASES["default"]["PORT"] = 5432
+
+# [END gaestd_py_django_database_config]
+# [END db_setup]
+
+# Use a in-memory sqlite3 database when testing in CI systems
+# TODO(glasnt) CHECK IF THIS IS REQUIRED because we're setting a val above
+# if os.getenv("TRAMPOLINE_CI", None):
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -125,7 +206,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "static/"
+STATICFILES_DIRS = []
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -140,8 +224,8 @@ SIMPLE_JWT = {
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = os.environ.get('WEATHER_EMAIL_FROM')
-EMAIL_HOST_USER = os.environ.get('WEATHER_EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('WEATHER_EMAIL_HOST_PASSWORD')
+# EMAIL_FROM = os.environ.get('WEATHER_EMAIL_FROM')
+# EMAIL_HOST_USER = os.environ.get('WEATHER_EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('WEATHER_EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
