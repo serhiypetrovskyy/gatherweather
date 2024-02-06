5290306c19a4c7244b9717a8e0a3052ab730aad6
serhiipetrovskyi <serhiypetrovskyy@gmail.com>
2024-02-06 12:32:02 +0200

    settings.py is updated, azure_storage.py is created to use Azure Storage

--- WeatherReminder/settings.py
+++ WeatherReminder/settings.py
@@ -206,10 +206,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = "static/"
-STATICFILES_DIRS = []
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = []
 
+DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
+STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
+
+AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
