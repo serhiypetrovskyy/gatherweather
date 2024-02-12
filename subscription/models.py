013f93c6052f82281fa08c48cb2121bf3a6bbe68
serhiipetrovskyi <serhiypetrovskyy@gmail.com>
2024-02-13 01:20:37 +0200

    A bug is fixed in models.py City model __str__ method

--- subscription/models.py
+++ subscription/models.py
@@ -6,7 +6,7 @@ class City(models.Model):
     country_code = models.CharField(max_length=2)
 
     def __str__(self):
-        return self.name
+        return self.city_name
 
 
 class Subscription(models.Model):
