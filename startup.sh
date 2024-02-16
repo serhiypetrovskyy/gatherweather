31fda284a8bc859a74af0dec8155ea9ebcb647ca
serhiipetrovskyi <serhiypetrovskyy@gmail.com>
2024-02-17 01:41:55 +0200

    startup.sh is updated. Fixed invalid syntax

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 export LANG=C.UTF-8
 
-gunicorn — bind=0.0.0.0 — timeout 600 config.wsgi & celery -A config worker -l
-INFO -B
+gunicorn --bind=0.0.0.0 --timeout 600 config.wsgi & celery -A config worker -l INFO -B
+
+
