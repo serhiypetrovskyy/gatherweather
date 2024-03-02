ecc7d7db16bdf7115c03f3e47339fe9632778341
serhiipetrovskyi <serhiypetrovskyy@gmail.com>
2024-03-02 13:32:44 +0200

    Test for subsription app are updated

--- subscription/tests/test_subscription_views.py
+++ subscription/tests/test_subscription_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 from subscription.models import Subscription, City
 from subscription.serializers import SubscriptionSerializer
@@ -15,6 +16,10 @@ class SubscriptionListViewTestCase(APITestCase):
         self.subscription = Subscription.objects.create(city_id=self.city, frequency=1, owner=self.user)
         self.url = reverse('subscription:subscriptions')
         self.client = APIClient()
+        self.schedule, self.created = IntervalSchedule.objects.get_or_create(
+                every=5,
+                period=IntervalSchedule.MINUTES
+            )
 
     def test_subscription_view_GET(self):
         self.client.force_authenticate(user=self.user)
@@ -39,6 +44,11 @@ class SubscriptionListViewTestCase(APITestCase):
         }
         response = self.client.post(self.url, data=data, format='json')
         new_subscription = Subscription.objects.get(pk=2)
+        task = PeriodicTask.objects.create(
+            interval=self.schedule,
+            name=self.subscription.id,
+            task='subscription.tasks.create_and_send_weather_report_task',
+        )
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(data["frequency"], new_subscription.frequency)
 
@@ -59,14 +69,23 @@ class SubscriptionDetailTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='user', password='userpassword', email='useremail@gmail.com')
         self.city = City.objects.create(city_name='Lviv', country_code='UA')
-        self.subscription = Subscription.objects.create(city_id=self.city, frequency=1, owner=self.user)
-        self.subscription_detail_url = reverse('subscription:subscription_detail', args=[self.subscription.id])
+        self.subscription_1 = Subscription.objects.create(city_id=self.city, frequency=1, owner=self.user)
+        self.subscription_detail_url = reverse('subscription:subscription_detail', args=[self.subscription_1.id])
         self.client = APIClient()
+        self.schedule, self.created = IntervalSchedule.objects.get_or_create(
+            every=5,
+            period=IntervalSchedule.MINUTES
+        )
+        self.task = PeriodicTask.objects.create(
+            interval=self.schedule,
+            name=self.subscription_1.id,
+            task='subscription.tasks.create_and_send_weather_report_task',
+        )
 
     def test_subscription_detail_view_GET(self):
         self.client.force_authenticate(user=self.user)
         response = self.client.get(self.subscription_detail_url)
-        serializer_data = SubscriptionSerializer([self.subscription, ], many=True).data
+        serializer_data = SubscriptionSerializer([self.subscription_1, ], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data[0], response.data)
 
@@ -86,9 +105,17 @@ class SubscriptionDetailTestCase(APITestCase):
         }
         city_name = data["city_id"]["city_name"]
         response = self.client.put(self.subscription_detail_url, data=data, format='json')
+        schedule, created = IntervalSchedule.objects.get_or_create(
+            every=10,
+            period=IntervalSchedule.MINUTES
+        )
+        task = PeriodicTask.objects.get(name=self.subscription_1.id)
+        task.interval = schedule
+        task.save()
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(data['frequency'], response.data['frequency'])
         self.assertEqual(city_name, response.data['city_id']['city_name'])
+        self.assertEqual(task.interval, schedule)
 
     def test_subscription_view_PUT_not_authenticated(self):
         data = {
