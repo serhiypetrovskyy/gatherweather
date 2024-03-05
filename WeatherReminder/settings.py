37a49cc81663cf1661ee320dabc7162743366123
serhiipetrovskyi <serhiypetrovskyy@gmail.com>
2024-03-05 12:24:32 +0200

    SMTP is changed to mailersend

--- WeatherReminder/settings.py
+++ WeatherReminder/settings.py
@@ -149,7 +149,8 @@ SIMPLE_JWT = {
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.mailersend.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
